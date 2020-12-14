3dd4d5005f1238d577debaa628f05f36abccf0b4
LokiGadd <48953600+LokiGadd@users.noreply.github.com>
2020-12-14 12:16:36 +0530

    Update 8.py

--- 8.py
+++ 8.py
@@ -1,6 +1,68 @@
 '''
 
-1. Write a Python program to print Hello World!
+8. Write a Python program to convert the given string into a morse code.
 
 '''
-print("Hello World!")
+def morse(letter):
+    if letter == 'A':
+        return ('. _')
+    if letter == 'B':
+        return ('_ . . .')
+    if letter == 'C':
+        return ('_ . _ .')
+    if letter == 'D':
+        return ('_ . .')
+    if letter == 'E':
+        return ('.')
+    if letter == 'F':
+        return ('. . _ .')
+    if letter == 'G':
+        return ('_ _ .')
+    if letter == 'H':
+        return ('. . . .')
+    if letter == 'I':
+        return ('. .')
+    if letter == 'J':
+        return ('. _ _ _')
+    if letter == 'K':
+        return ('_ . _')
+    if letter == 'L':
+        return ('. _ . .')
+    if letter == 'M':
+        return ('_ _')
+    if letter == 'N':
+        return ('_ .')
+    if letter == 'O':
+        return ('_ _ _')
+    if letter == 'P':
+        return ('. _ _ .')
+    if letter == 'Q':
+        return ('_ _ . _')
+    if letter == 'R':
+        return ('. _ .')
+    if letter == 'S':
+        return ('. . .')
+    if letter == 'T':
+        return ('_')
+    if letter == 'U':
+        return ('. . _')
+    if letter == 'V':
+        return ('. . . _')
+    if letter == 'W':
+        return ('. _ _')
+    if letter == 'X':
+        return ('_ . . _')
+    if letter == 'Y':
+        return ('_ . _ _')
+    if letter == 'Z':
+        return ('_ _ . .')
+
+print ("\n")
+word = input("Enter the sentence : ")
+for i in range(0,len(word)):
+    if word[i] == ' ':
+        print ("     ", end='')
+    else:
+        print(morse(word[i]), end='')
+        print ("  ", end='')
+print ("\n")
