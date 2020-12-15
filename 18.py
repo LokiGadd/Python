d490f115324272cce2b04e0bb889e1360a0be7eb
LokiGadd <48953600+LokiGadd@users.noreply.github.com>
2020-12-15 12:48:06 +0530

    Update 18.py

--- 18.py
+++ 18.py
@@ -1,6 +1,8 @@
 # 15 December 2020
 '''
 
+Problem 18
+
 Given the names and grades for each student in a class of N students, store them in a nested list and print the name(s) of any student(s) having the second lowest grade.
 
 '''
